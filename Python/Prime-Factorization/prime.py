36daafe5bf2cdf0ba6da151ccb9a08cb70f3283a
RotomReploid <104281650+RotomReploid@users.noreply.github.com>
2022-05-06 08:21:00 -0400

    Create prime.py

---
+++ Python/Prime-Factorization/prime.py
@@ -0,0 +1,24 @@
+# python program to print prime factors
+import site
+import math
+def primefactors(n):
+   #even number divisible
+   while n % 2 == 0:
+      print (2),
+      n = n / 2
+    
+   #n became odd
+   for i in range(3,int(math.sqrt(n))+1,2):
+     
+      while (n % i == 0):
+         print (i)
+         n = n / i
+    
+   if n > 2:
+      print (n)
+while(1==1): 
+    n = input("Enter the number for calculating the prime factors :\n")
+    if (n != "done"):
+        primefactors(int(n))
+    else:
+        quit()
