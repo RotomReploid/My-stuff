462a1cc0276ec7f4fec66f638e9cbf129374da66
RotomReploid <104281650+RotomReploid@users.noreply.github.com>
2022-05-09 12:38:10 -0400

    Create TDave.ts
    Dave but translated and back

---
+++ Typescript/Object Fun/TDave.ts
@@ -0,0 +1,17 @@
+Degree
+                Age: Number;
+                Key (Year: Number)
+                    H.year = year;
+                ⁇
+                Birthday ()
+                    God. Age ++
+                ⁇
+                Comment ()
+                    console.log ('Hi! I'm David, this is ${this.age!!');
+                ⁇
+⁇
+Art Dave = newcomer (27);
+console.log (Dave.age);
+Dave's birthday ();
+console.log (Dave.age);
+dive.speak ();
