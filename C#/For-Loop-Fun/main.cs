fd1c580467625dedd30ea2b40b047db90146c735
RotomReploid <104281650+RotomReploid@users.noreply.github.com>
2022-07-23 17:09:56 -0400

    Create main.cs

---
+++ C#/For-Loop-Fun/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    static public void Main()
+    {
+        for (int bottleNumber = 100; 1 < bottleNumber; bottleNumber--)
+        {
+            Console.WriteLine("{0} bottles of beer on the wall, {0} bottles of beer", bottleNumber);
+            Console.WriteLine($"Take one down and pass it around, {bottleNumber - 1} bottles of beer on the wall");
+        }
+        Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
+        Console.WriteLine("Take one down and pass it around, no bottles of beer on the wall");
+    }
+}
