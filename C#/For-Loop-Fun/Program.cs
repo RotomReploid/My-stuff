6f7484097cfd4c27c2ed6a5aae19c56fc633b977
JAYDEN MUGEMUZI (S963546) <jamugemuzi@gmail.com>
2022-09-13 13:23:32 -0400

    epiku

--- C#/For-Loop-Fun/Program.cs
+++ C#/For-Loop-Fun/Program.cs
@@ -7,9 +7,10 @@ public class Program
         for (int bottleNumber = 100; 1 < bottleNumber; bottleNumber--)
         {
             Console.WriteLine("{0} bottles of beer on the wall, {0} bottles of beer", bottleNumber);
-            Console.WriteLine($"Take one down and pass it around, {bottleNumber - 1} bottles of beer on the wall");
+            Console.WriteLine("Take one down and pass it around, {0} bottles of beer on the wall", bottleNumber - 1);
         }
         Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
         Console.WriteLine("Take one down and pass it around, no bottles of beer on the wall");
+        Console.ReadKey();
     }
 }
