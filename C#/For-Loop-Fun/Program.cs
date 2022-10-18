67ef8f4ccff9a91c6b2bd256549d74d71401f255
RotomReploid <104281650+RotomReploid@users.noreply.github.com>
2022-10-18 13:12:50 -0400

    adsads

--- C#/For-Loop-Fun/Program.cs
+++ C#/For-Loop-Fun/Program.cs
@@ -8,8 +8,9 @@ public class Program
         for (int bottleNumber = 100; 1 < bottleNumber; bottleNumber--)
         {
             Console.WriteLine("{0} bottles of beer on the wall, {0} bottles of beer", bottleNumber);
-            Console.WriteLine("Take one down and pass it around, {0} bottles of beer on the wall", bottleNumber - 1);
             Thread.Sleep(1000);
+            Console.WriteLine("Take one down and pass it around, {0} bottles of beer on the wall", bottleNumber - 1);
+            Thread.Sleep(4000);
         }
         Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
         Console.WriteLine("Take one down and pass it around, no bottles of beer on the wall");
