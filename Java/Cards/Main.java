1d3e9acca2cf2ec1deb5115ae771926469956d62
RotomReploid <104281650+RotomReploid@users.noreply.github.com>
2022-11-15 13:15:07 -0500

    jfuiewfjmfwelk

--- Java/Cards/Main.java
+++ Java/Cards/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 class Main {
 	public static void main(String[] args) {
 		Deck deck = new Deck();
-		Cards.Suit.values();
+		System.out.println(Cards.Suit.values());
 		/*for (Suit boot : Cards.Suit.values()){
 			for (Rank plank : Cards.Rank.values()){
 				deck.add(new Card(plank, boot));
